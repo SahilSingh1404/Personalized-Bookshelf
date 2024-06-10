14767910cc421c4750d4ebb918f13085d51ab81e
SSingh <ssingh@kenmarkitan.com>
2024-06-10 23:25:46 +0530

    Bookshelf changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
 import Bookshelf from './bookshelf';
-import MyBookshelfPage from './mybookshelf';
 import { BookshelfProvider } from './BookshelfContext';
 import './App.css';
 
@@ -17,7 +16,6 @@ const App = () => {
                         <Route path="/" element={<SignupPage />} />
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/book" element={<Bookshelf />} />
-                        <Route path="/my-bookshelf" element={<MyBookshelfPage />} />
                         <Route exact path="/" element={<h1>Welcome to my App</h1>} />
                     </Routes>
                 </div>
