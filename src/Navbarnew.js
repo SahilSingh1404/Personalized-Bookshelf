afa088f7be014c1885234f91955c824e8784d190
SSingh <ssingh@kenmarkitan.com>
2024-06-10 23:38:56 +0530

    Resolved errors

--- src/Navbarnew.js
+++ src/Navbarnew.js
@@ -32,38 +32,3 @@ export default function ButtonAppBar() {
     </Box>
   );
 }
-
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import logo from './logo.png'; 
-
-export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <div style={{ borderRadius: '50%', overflow: 'hidden', marginRight: '10px' }}>
-            <img src={logo} alt="Logo" style={{ height: '40px', width: '40px', objectFit: 'cover' }} /> 
-          </div>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Personalized Bookshelf
-          </Typography>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
